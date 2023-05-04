41eb02d77526f8da9e06544c147622f46ebeb7bd
Thomas Roovers <troovers@e-sites.nl>
2023-05-04 10:21:35 +0200

    Added Formulae

---
+++ Formula/flttr.rb
@@ -0,0 +1,14 @@
+class Foo < Formula
+    desc "An Unlock wrapper for Flutter"
+    homepage "https://github.com/troovers/homebrew-flttr"
+    url "https://github.com/troovers/homebrew-flttr.git"
+    version: "0.1.0"
+    
+    def install
+        bin.install "bin/flttr"
+    end
+  
+    test do
+        system "false"
+    end
+end
