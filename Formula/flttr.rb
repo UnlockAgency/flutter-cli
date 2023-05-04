82d294de62ac278cf6d226f1f7dcf67778c9de62
Thomas Roovers <troovers@e-sites.nl>
2023-05-04 10:28:55 +0200

    Formulae rename

--- Formula/flttr.rb
+++ Formula/flttr.rb
@@ -1,4 +1,4 @@
-class Foo < Formula
+class Flttr < Formula
     desc "An Unlock wrapper for Flutter"
     homepage "https://github.com/troovers/homebrew-flttr"
     url "https://github.com/troovers/homebrew-flttr.git"
